c50eb2b0259f1cdf31ff1b5325fd27428d41d993
AlRashid Issa <147489627+AlRashidIssa@users.noreply.github.com>
2024-10-03 06:28:38 +0000

    Create Class for LogIn and Regiester

---
+++ src/api/fracture_detection/core/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+class LoginIn(AuthenticationForm):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'autofocus': True}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+class LogUp(UserCreationForm):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'autofocus': True}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username',)
